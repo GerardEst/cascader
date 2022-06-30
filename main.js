5a8fe5a29069d85ab58122b313045c69de5fe2bb
Gerard <gesteve.12@gmail.com>
2022-06-30 12:50:15 +0200

    Adaptación para minWidth en vez de col

--- main.js
+++ main.js
@@ -1,18 +1,14 @@
 import {cascade, removeCascade} from './cascader.js'
 
 cascade('#cascada', {
-    columns: 3,
-    gap: 20,
-    //verticalGap : 30
-    //horizontalGap : 30
+    minWidth: 300, 
+    gap: 20
 })
 
 document.querySelector('#remove').addEventListener('click', () => removeCascade())
 document.querySelector('#cascade').addEventListener('click', () => {
     cascade(document.querySelector('#cascada'), {
-        columns: 3,
-        gap: 20,
-        //verticalGap : 30
-        //horizontalGap : 30
+        minWidth: 300,
+        gap: 20
     })
 })
