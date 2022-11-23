0a6bfa43d8fcb18437f9ffc685ae86fe3d9df949
Gerard Esteve <gesteve.12@gmail.com>
2022-11-23 18:24:25 +0100

    Typescript en cascader

---
+++ examples/main.js
@@ -0,0 +1,5 @@
+import { cascade } from "../build/cascader-min.js";
+let options = {
+  minWidth: 350
+};
+cascade("#container", options);
--- src/cascader.ts
+++ src/cascader.ts
@@ -1,34 +1,39 @@
-let container_width = 0
-let rows = 0
-let cols = 0
-let vertical_gap = 0
-let horizontal_gap = 0
-let minWidth = 0
-let element = null
-let bricks = null
+let container_width: number
+let rows: number
+let cols: number
+let vertical_gap: number
+let horizontal_gap: number
+let minWidth: number
+let container: HTMLElement | null
+let bricks: any
 
-let resizeObserver = new ResizeObserver( (entries) => {
+type CascadeContainer = string | HTMLElement
+export interface CascadeOptions {
+  minWidth: number,
+  gap?: number,
+  verticalGap?: number,
+  horizontalGap?: number
+}
+
+let resizeObserver = new ResizeObserver( () => {
   init()
 })
 
-export function cascade(container_element, options){
+export function cascade(container_container: CascadeContainer, options: CascadeOptions){
+  
+  horizontal_gap = options.horizontalGap || options.gap || 20
+  vertical_gap = options.verticalGap || options.gap || 20
+  minWidth = options.minWidth || 200
 
-    horizontal_gap = options.horizontalGap || options.gap || 20
-    vertical_gap = options.verticalGap || options.gap || 20
-    minWidth = options.minWidth || 200
-    element = typeof container_element === 'string' ? document.querySelector(container_element) : container_element
-    container_width = element.offsetWidth
-    cols = Math.floor(container_width / minWidth)
+  container = typeof container_container === 'string' ? document.querySelector(container_container) : container_container
+  container_width = container!.offsetWidth
+  cols = Math.floor(container_width / minWidth)
 
-    // TODO -> Com que només ID?? Que es pugui amb classes també home. 
-    // TODO -> si hi ha un error, que no bloquegi el js de la pagina
-    if(!element.id) console.error('Cascader: Container needs an id')
+  bricks = container!.children // Esto seguro que es una HTMLCollection
 
-    bricks = document.querySelectorAll(`#${element.id} > *`)
+  container!.style.position = 'relative'
 
-    element.style.position = 'relative'
-
-    init()
+  init()
 }
   
 function init(){
@@ -38,7 +43,7 @@ function init(){
 
 export function removeCascade(){  
   resizeObserver.disconnect()
-  element.removeAttribute('style')
+  container!.removeAttribute('style')
   for(let brick of bricks) brick.removeAttribute('style')
 }
 
@@ -46,31 +51,32 @@ function positionBricks(){
   for(let i=0; i<bricks.length; i++){
     
     // Recalculate container_width when resize and redistribute columns
-    container_width = element.offsetWidth
+    container_width = container!.offsetWidth
     cols = Math.floor(container_width / minWidth)
 
-    const brick = bricks[i]
+    const brick:HTMLElement = bricks[i] // Typescript cree que bricks[i] es un Element. bricks es HTMLCollection. 
+    //Tengo que decirle que lo de dentro no son Element sino HTMLElement, pero no me deja hacer HTMLCollectionOf
 
-    const brick_row = Math.floor(i/cols)
-    const brick_col = i%cols
-    const brick_width = 100/cols
-    const gap_reduce = horizontal_gap*(cols-1)/cols
+    const brick_row:number = Math.floor(i/cols)
+    const brick_col:number = i%cols
+    const brick_width:number = 100/cols
+    const gap_reduce:number = horizontal_gap*(cols-1)/cols
     if(brick_row > rows) rows = brick_row
 
     brick.style.position = 'absolute'
-    brick.setAttribute('row', brick_row)
-    brick.setAttribute('col', brick_col)
+    brick.setAttribute('row', `${brick_row}`)
+    brick.setAttribute('col', `${brick_col}`)
 
     // Horizontal positioning
     brick.style.width = `calc(${brick_width}% - ${gap_reduce}px)`
     brick.style.left = `calc(${brick_width*brick_col}% + ${horizontal_gap/cols * brick_col}px`
 
     // Vertical positioning: sume of the precedent bricks in the column 
-    const bricks_in_col = element.querySelectorAll(`[col='${brick_col}']`)
+    const bricks_in_col = container!.querySelectorAll(`[col='${brick_col}']`)
     let brick_column_height = 0
 
     for(let brick_in_col of bricks_in_col){
-      if(brick_in_col.getAttribute('row') > brick_row) continue
+      if(Number(brick_in_col.getAttribute('row')) > brick_row) continue
       brick_column_height = brick_column_height + brick_in_col.offsetHeight + vertical_gap
     }
 
@@ -88,7 +94,7 @@ function setContainerHeight(){
   let largest_col = 0
   
   for(let i=0; i<cols; i++){
-    const col = element.querySelectorAll(`[col='${i}']`)
+    const col = container!.querySelectorAll(`[col='${i}']`)
     
     let total_height = 0
     
@@ -101,5 +107,5 @@ function setContainerHeight(){
     if(total_height > largest_col) largest_col = total_height
   }
 
-  element.style.height = largest_col+'px'
+  container!.style.height = largest_col+'px'
 }
